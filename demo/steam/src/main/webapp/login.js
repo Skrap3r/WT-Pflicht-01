24dc5966ba7fad927cef7fb64b6b5077de7eacb5
Jannes <jannes.lensch@web.de>
2022-04-28 14:59:44 +0200

    Login Seite

---
+++ demo/steam/src/main/webapp/login.js
@@ -0,0 +1,13 @@
+function validate(){
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    
+    if(username=="admin"&&password=="123"){
+        alert("login succesfull");
+        window.href="steam.html";
+    } else {
+        alert("login failed")
+    }
+}
+
+document.querySelector("#btn1").addEventListener("click", (event) => validate());
